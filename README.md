631e295c3b737274051cebc5022471b5b5af9666
Kyuoraku <nico.99.godoy@gmail.com>
2020-10-20 15:13:48 -0300

    Hosteado

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+This project is live rn! To try it, go [This link](https://img-search-pixabay.web.app/)!
+
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
 
 ## Available Scripts
---
+++ build/precache-manifest.4a5a98b772f6e110d9b0c5427d02a7f9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd39ab23d85b1204e13dd98f742b44fe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cf09ce6c36594a787bb5",
+    "url": "/static/css/main.4883fd05.chunk.css"
+  },
+  {
+    "revision": "f37634415ae7fe09faca",
+    "url": "/static/js/2.6d7e2237.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.6d7e2237.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cf09ce6c36594a787bb5",
+    "url": "/static/js/main.b967e0df.chunk.js"
+  },
+  {
+    "revision": "d54fdc3452b242e5cbae",
+    "url": "/static/js/runtime-main.cc83ca70.js"
+  }
+]);
